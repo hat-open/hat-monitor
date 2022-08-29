6d56275bc5ce50847ab72dfaefef983059fa34dc
bozo.kopic <bozo@kopic.xyz>
2022-08-29 21:14:52 +0200

    sbs update

--- src_py/hat/monitor/common.py
+++ src_py/hat/monitor/common.py
@@ -55,8 +55,8 @@ class MsgServer(typing.NamedTuple):
 
 def blessing_req_to_sbs(blessing: BlessingReq) -> sbs.Data:
     """Convert blessing request to SBS data"""
-    return {'token': _value_to_sbs_maybe(blessing.token),
-            'timestamp': _value_to_sbs_maybe(blessing.timestamp)}
+    return {'token': _value_to_sbs_optional(blessing.token),
+            'timestamp': _value_to_sbs_optional(blessing.timestamp)}
 
 
 def blessing_req_from_sbs(data: sbs.Data) -> BlessingReq:
@@ -67,7 +67,7 @@ def blessing_req_from_sbs(data: sbs.Data) -> BlessingReq:
 
 def blessing_res_to_sbs(res: BlessingRes) -> sbs.Data:
     """Convert blessing response to SBS data"""
-    return {'token': _value_to_sbs_maybe(res.token),
+    return {'token': _value_to_sbs_optional(res.token),
             'ready': res.ready}
 
 
@@ -81,8 +81,8 @@ def component_info_to_sbs(info: ComponentInfo) -> sbs.Data:
     """Convert component info to SBS data"""
     return {'cid': info.cid,
             'mid': info.mid,
-            'name': _value_to_sbs_maybe(info.name),
-            'group': _value_to_sbs_maybe(info.group),
+            'name': _value_to_sbs_optional(info.name),
+            'group': _value_to_sbs_optional(info.group),
             'data': json.encode(info.data),
             'rank': info.rank,
             'blessingReq': blessing_req_to_sbs(info.blessing_req),
@@ -134,8 +134,8 @@ def msg_server_from_sbs(data: sbs.Data) -> MsgServer:
                                  for info in data['components']])
 
 
-def _value_to_sbs_maybe(value):
-    return ('Just', value) if value is not None else ('Nothing', None)
+def _value_to_sbs_optional(value):
+    return ('value', value) if value is not None else ('none', None)
 
 
 def _value_from_sbs_maybe(maybe):
